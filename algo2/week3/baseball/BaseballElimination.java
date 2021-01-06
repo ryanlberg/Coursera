8ddf492ec520dc507797f585228a6041a36c1008
Ryan Berg <ryanmcberg@gmail.com>
2021-01-06 09:29:42 -0500

    updated main function and added exceptions

--- algo2/week3/baseball/BaseballElimination.java
+++ algo2/week3/baseball/BaseballElimination.java
@@ -1,8 +1,10 @@
 /* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
+ *  Name: Ryan Berg
+ *  Date: 1/6/2021
+ *  Description: BaseBall elmination as descrbed at https://coursera.cs.princeton.edu/algs4/assignments/baseball/specification.php
  **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
 
 public class BaseballElimination {
 
@@ -19,30 +21,56 @@ public class BaseballElimination {
     }
 
     public int wins(String team) {
-
+        if (team == null) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int losses(String team) {
-
+        if (team == null) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int remaining(String team) {
-
+        if (team == null) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public int agains(String team1, String team2) {
-
+    public int against(String team1, String team2) {
+        if (team1 == null || team2 == null) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public boolean isEliminated(String team) {
+        if (team == null) {
+            throw new IllegalArgumentException();
+        }
 
     }
 
-    public Iterable<String> certificatieofElimination(String team) {
-        
+    public Iterable<String> certificateOfElimination(String team) {
+        if (team == null) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public static void main(String[] args) {
+        BaseballElimination division = new BaseballElimination(args[0]);
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team)) {
+                    StdOut.print(t + " ");
+                }
+                StdOut.println("}");
+            }
+            else {
+                StdOut.println(team + " is not eliminated");
+            }
+        }
 
     }
 }
