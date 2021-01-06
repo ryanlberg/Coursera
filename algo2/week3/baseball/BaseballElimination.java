4039f9dc3d6b807d841d2664ad7a1702955daa60
Ryan Berg <ryanmcberg@gmail.com>
2021-01-06 09:14:29 -0500

    week3 initial commit

---
+++ algo2/week3/baseball/BaseballElimination.java
@@ -0,0 +1,48 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class BaseballElimination {
+
+    public BaseballElimination(String filename) {
+
+    }
+
+    public int numberOfTeams() {
+
+    }
+
+    public Iterable<String> teams() {
+
+    }
+
+    public int wins(String team) {
+
+    }
+
+    public int losses(String team) {
+
+    }
+
+    public int remaining(String team) {
+
+    }
+
+    public int agains(String team1, String team2) {
+
+    }
+
+    public boolean isEliminated(String team) {
+
+    }
+
+    public Iterable<String> certificatieofElimination(String team) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
